0d4715c797ca87158ba83270108e87ff555993a0
Benoit Caccinolo <benoit.caccinolo@axa.fr>
2025-05-21 15:31:13 +0200

    first commit

--- src/main/java/com/example/demo_mcp_client/WeatherService.java
+++ src/main/java/com/example/demo_mcp_client/WeatherService.java
@@ -3,7 +3,6 @@ package com.example.demo_mcp_client;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.tool.annotation.Tool;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
@@ -17,12 +16,13 @@ import java.util.List;
 public class WeatherService {
 
     private static final String API_URL = "https://api.open-meteo.com/v1/forecast";
-    private static final String API_REQUEST_PARAMS = "?latitude=%f&longitude=%f&hourly=temperature_2m&forecast_days=1&format=json&timeformat=unixtime";
+    private static final String API_REQUEST_PARAMS = "?latitude=%s&longitude=%s&hourly=temperature_2m&forecast_days=1&format=json&timeformat=unixtime";
 
     private final RestTemplate restTemplate;
 
     @Tool(description = "Get current temperature for a specific latitude/longitude")
     public String temperature(double latitude, double longitude) {
+        log.info("Get current temperature for latitude {} and longitude {}", latitude, longitude);
         // input validation
         if (latitude > 90 || latitude < -90 || longitude > 90 || longitude < -90) {
             return "Error: invalid latitude/longitude";
@@ -32,15 +32,21 @@ public class WeatherService {
         return weatherResponse.getHourly().getTemperature2m().getFirst().toString();
     }
 
-    public WeatherResponse getWeather(double latitude, double longitude) {
-        String url = String.format(API_URL + API_REQUEST_PARAMS, latitude, longitude);
+    public WeatherResponse getWeather(Double latitude, Double longitude) {
+        String url = String.format(API_URL + API_REQUEST_PARAMS,
+                                   latitude.toString().replace(",", "."),
+                                   longitude.toString().replace(",", "."));
         log.info(url);
-        return restTemplate.exchange(
-                url,
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<WeatherResponse>>() {}
-        ).getBody().getFirst();
+        return restTemplate.getForObject(url, WeatherResponse.class);
+
+        // For a list of Response
+        //                restTemplate.exchange(
+        //                        url,
+        //                        HttpMethod.GET,
+        //                        null,
+        //                        new ParameterizedTypeReference<List<WeatherResponse>>() {
+        //                        }
+        //                ).getBody().getFirst();
     }
 
 
